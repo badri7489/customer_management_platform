22d7abf3275c9e085ac33ab35bdd2b6821834c83
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-26 19:46:13 +0530

    Search Bar Created

--- customer_management_platform/accounts/views.py
+++ customer_management_platform/accounts/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
 from django.forms import inlineformset_factory
+from .filters import OrderFilter
 
 # Create your views here.
+
+def registerPage(request):
+    context = {}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def home(request):
     orders = Order.objects.all()
@@ -35,7 +44,16 @@ def customer(request, pk_test):
 
     orders = customer.order_set.all()
     order_count = orders.count()
-    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+
+    myfilter = OrderFilter(request.GET, queryset=orders)
+    orders = myfilter.qs
+
+    context = {
+        'customer': customer, 
+        'orders': orders, 
+        'order_count': order_count,
+        'myfilter': myfilter,
+    }
     return render(request, 'accounts/customer.html', context)
 
 def createOrder(request, pk):
--- customer_management_platform/customer_management_platform/settings.py
+++ customer_management_platform/customer_management_platform/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'django_filters',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
