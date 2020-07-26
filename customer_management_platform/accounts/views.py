6282d3c1589e1d8bb4d66730e195d040e9dcbeb0
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-26 23:20:31 +0530

    Registration Page Added

--- customer_management_platform/accounts/views.py
+++ customer_management_platform/accounts/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import *
-from .forms import OrderForm
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import inlineformset_factory
-from .filters import OrderFilter
+from django.contrib import messages
 
 # Create your views here.
+from .models import *
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
 
 def registerPage(request):
-    context = {}
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+            
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
