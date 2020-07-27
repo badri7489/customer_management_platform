f5a86bd3f931d0bf91074fb54f8d1659d4c24df0
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-27 21:38:29 +0530

    Google SMTP Added

--- customer_management_platform/customer_management_platform/settings.py
+++ customer_management_platform/customer_management_platform/settings.py
@@ -131,3 +131,13 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+#SMTP Configuration
+#Simple Mail Transfer Protocol
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'badrisharma333@gmail.com'
+EMAIL_HOST_PASSWORD = 'shitty744889'
