8c188afcd8cf86e755fbf28a04275a2e4128b5cd
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-27 17:35:13 +0530

    Account Settings Page Added

--- customer_management_platform/accounts/views.py
+++ customer_management_platform/accounts/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import Group
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_user, admin_only
 
@@ -103,7 +103,14 @@ def userPage(request):
 @login_required(login_url='login')
 @allowed_user(allowed_roles=['customer'])
 def accountSettings(request):
-    context = {}
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+    
+    if request.method == 'POST':
+        form = CustomerForm(request.POST,request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
     return render(request, 'accounts/account_settings.html', context)
 
 @login_required(login_url='login')
