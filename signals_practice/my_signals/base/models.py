506b020aa2409010e62380c33f96b6c240b39b7a
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-27 20:21:15 +0530

    Signals Understood :)

--- signals_practice/my_signals/base/models.py
+++ signals_practice/my_signals/base/models.py
@@ -10,25 +10,4 @@ class Profile(models.Model):
     phone = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
-        return self.first_name
-
-def create_profile():
-    '''
-        if created:
-            Profile.objects.create(user=instance)
-            print('Profile Created!!')
-    '''
-
-def update_profile():
-    '''
-        if created == False:
-            print()
-            try:
-                instance.profile.save()
-                print("Profile Update!!")
-            except:
-                Profile.objects.create(user=instance)
-                print('Profile created for existing user!!')
-
-    '''
-    
+        return str(self.user)
